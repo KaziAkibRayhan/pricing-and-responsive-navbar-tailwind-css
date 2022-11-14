53525d961f6053e09fd107dd2cf924a3ccedc8aa
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-14 15:01:34 +0600

    reachart added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import AssignmentsMarks from "./components/AssignmentsMarks/AssignmentsMarks";
 import Navbar from "./components/Navbar/Navbar";
 import Pricing from "./components/Pricing/Pricing";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar></Navbar>
       <Pricing></Pricing>
+      <AssignmentsMarks></AssignmentsMarks>
     </div>
   );
 }
